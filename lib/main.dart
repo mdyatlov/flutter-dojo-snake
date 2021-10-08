10017db118d5e837aa99cc07fd1367d73695367d
Vincent <jouanne.vincent@gmail.com>
2021-10-08 15:46:15 +0200

    feat(game): add full game

--- lib/main.dart
+++ lib/main.dart
@@ -1,3 +1,6 @@
+import 'dart:async';
+import 'dart:math';
+
 import 'package:flutter/material.dart';
 
 void main() {
@@ -28,15 +31,176 @@ class MyHomePage extends StatefulWidget {
 }
 
 class _MyHomePageState extends State<MyHomePage> {
+  static List<int> snakePosition = [45, 65, 85, 105, 125];
+  int food = Random().nextInt(700);
+  var direction = 'down';
+
+  void generateNextFood() {
+    food = Random().nextInt(700);
+  }
+
+  void startGame() {
+    const duration = Duration(milliseconds: 300);
+    snakePosition = [45, 65, 85, 105, 125];
+
+    Timer.periodic(duration, (Timer timer) {
+      updateSnake();
+      if (gameOver()) {
+        timer.cancel();
+        _showGameOverPopup();
+      }
+    });
+  }
+
+  bool gameOver() {
+    for (int i = 0; i < snakePosition.length; i++) {
+      int count = 0;
+      for (int j = 0; j < snakePosition.length; j++) {
+        if (snakePosition[i] == snakePosition[j]) {
+          count++;
+        }
+        if (count == 2) {
+          return true;
+        }
+      }
+    }
+
+    return false;
+  }
+
+  void _showGameOverPopup() {
+    showDialog(
+      context: context,
+      builder: (BuildContext context) {
+        return AlertDialog(
+          title: const Text('GAME OVER'),
+          content: Text('You\'re score: ' + snakePosition.length.toString()),
+          actions: <Widget>[
+            TextButton(
+              child: const Text('Play again !'),
+              onPressed: () {
+                startGame();
+                Navigator.of(context).pop();
+              },
+            )
+          ],
+        );
+      },
+    );
+  }
+
+  void updateSnake() {
+    setState(() {
+      switch (direction) {
+        case 'down':
+          if (snakePosition.last > 740) {
+            snakePosition.add(snakePosition.last + 20 - 760);
+          } else {
+            snakePosition.add(snakePosition.last + 20);
+          }
+          break;
+        case 'up':
+          if (snakePosition.last < 20) {
+            snakePosition.add(snakePosition.last - 20 + 760);
+          } else {
+            snakePosition.add(snakePosition.last - 20);
+          }
+          break;
+        case 'left':
+          if (snakePosition.last % 20 == 0) {
+            snakePosition.add(snakePosition.last - 1 + 20);
+          } else {
+            snakePosition.add(snakePosition.last - 1);
+          }
+          break;
+        case 'right':
+          if ((snakePosition.last + 1) % 20 == 0) {
+            snakePosition.add(snakePosition.last + 1 + 20);
+          } else {
+            snakePosition.add(snakePosition.last + 1);
+          }
+          break;
+      }
+
+      if (snakePosition.last == food) {
+        generateNextFood();
+      } else {
+        snakePosition.removeAt(0);
+      }
+    });
+  }
+
   @override
   Widget build(BuildContext context) {
-    return const Scaffold(
+    return Scaffold(
       backgroundColor: Colors.black,
-      body: Center(
-        child: Text(
-          "Bonjour le Dojo !",
-          style: TextStyle(fontSize: 24, color: Colors.white),
-        ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            child: GestureDetector(
+              onVerticalDragUpdate: (details) {
+                if (direction != 'up' && details.delta.dy > 0) {
+                  direction = 'down';
+                } else if (direction != 'down' && details.delta.dy < 0) {
+                  direction = 'up';
+                }
+              },
+              onHorizontalDragUpdate: (details) {
+                if (direction != 'left' && details.delta.dx > 0) {
+                  direction = 'right';
+                } else if (direction != 'right' && details.delta.dx < 0) {
+                  direction = 'left';
+                }
+              },
+              child: GridView.builder(
+                physics: const NeverScrollableScrollPhysics(),
+                itemCount: 760,
+                gridDelegate: const SliverGridDelegateWithFixedCrossAxisCount(
+                  crossAxisCount: 20,
+                ),
+                itemBuilder: (BuildContext context, int index) {
+                  if (snakePosition.contains(index)) {
+                    return Container(
+                      padding: const EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(color: Colors.white),
+                      ),
+                    );
+                  } else if (index == food) {
+                    return Container(
+                      padding: const EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(color: Colors.green),
+                      ),
+                    );
+                  } else {
+                    return Container(
+                      padding: const EdgeInsets.all(2),
+                      child: ClipRRect(
+                        borderRadius: BorderRadius.circular(5),
+                        child: Container(color: Colors.grey[900]),
+                      ),
+                    );
+                  }
+                },
+              ),
+            ),
+          ),
+          Padding(
+            padding: const EdgeInsets.only(bottom: 20.0, top: 20.0),
+            child: Center(
+              child: GestureDetector(
+                onTap: startGame,
+                child: const Text(
+                  'START',
+                  style: TextStyle(color: Colors.white),
+                ),
+              ),
+            ),
+          )
+        ],
       ),
     );
   }
